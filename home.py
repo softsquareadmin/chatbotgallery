48e9185be7b3e1a125fb041f3c71815dc0c5dac3
Manikandan P <manikandanp@softsquare.biz>
2024-08-09 15:59:38 +0530

    added image

--- home.py
+++ home.py
@@ -36,7 +36,7 @@ with col1:
      hasClicked = card(
     key='1',
     title=["AGrid", " ", " "],
-    text = "",
+    text = "chatbot",
     # text="Are you looking for a faster, more efficient way to manage your data on Salesforce? Look no further than AGrid. Our lightning-fast grids and powerful features streamline workflows, boost productivity, and enhance data quality across your organization. Experience the ultimate Salesforce data management solution with AGrid. Chat with us to explore more about the product.",
     image=f"""https://raw.githubusercontent.com/softsquareadmin/chatbotgallery/main/AGrid%20Logo%203.png""",
     url="https://agridchatbot-vcd4ehhmbkb5x54enrsrbi.streamlit.app/",
@@ -56,8 +56,8 @@ with col1:
 with col2:
     hasClicked = card(
     key='2',
-    title=["User 360", " ", " "],
-    text = "",
+    title=["User 360"],
+    text = "chatbot",
     # text="Are you seeking an efficient solution for user management within Salesforce? Look no further than User360. Our platform offers robust features to streamline workflows, enhance productivity, and elevate data quality across your organization. User360 automates user management tasks, ensuring a seamless and efficient user experience. Chat with us to explore more about the product.",
     image=f"""https://raw.githubusercontent.com/softsquareadmin/chatbotgallery/main/User360.png""",
     url="https://user360bot-4pxrknjb8zo2fxgz2hafug.streamlit.app/",
@@ -75,11 +75,36 @@ with col2:
 with col3:
     hasClicked = card(
     key='3',
-    title=["Media Manager", " "],
-    text = "",
+    title=["Media Manager"],
+    text = "chatbot",
     # text="Media Manager, designed for the Lightning Experience, simplifies file management within Salesforce records. With built-in viewer modes, file controls, and powerful search capabilities, it enhances productivity and customization to meet your business needs. Chat with us to explore more about the product.",
     image="https://raw.githubusercontent.com/softsquareadmin/chatbotgallery/main/Media%20Manager%203.png",
     url="",
+    styles={
+        "card": {
+            # "width": "400px",
+            # "height": "500px",
+            "border-radius": "20px",
+            "box-shadow": "0 0 10px rgba(17, 95, 250, 0.88)", 
+                       
+        },
+        # "filter": {
+        #     "background-color": "rgba(255, 255, 255, 255)" 
+            
+        # }
+    }
+)
+
+col11, col21, col31 = st.columns(3)
+
+with col11:
+    hasClicked = card(
+    key='21',
+    title=["App Analytics"],
+    text = "Dashboard",
+    # text="Media Manager, designed for the Lightning Experience, simplifies file management within Salesforce records. With built-in viewer modes, file controls, and powerful search capabilities, it enhances productivity and customization to meet your business needs. Chat with us to explore more about the product.",
+    image="https://raw.githubusercontent.com/softsquareadmin/chatbotgallery/main/analytics-icon.png",
+    url="https://devappanalystics-eve9yjazayqvexttmutt2h.streamlit.app/",
     styles={
         "card": {
             # "width": "400px",
