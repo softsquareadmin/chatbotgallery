804ea53104a97ee929647563045268521b541141
Manikandan P <manikandanp@softsquare.biz>
2024-08-22 15:35:50 +0530

    UI chnages

--- home.py
+++ home.py
@@ -7,8 +7,8 @@ st.set_page_config(
     layout="wide"
 )
 
-with open("style.css", "r") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+# with open("style.css", "r") as f:
+#     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 hide_st_style = """ 
